166cdaff69f3e9df9d740b22ca35a3b92460470b
ikaroamorim <ikaro.amorim@gmail.com>
2021-12-19 21:27:00 -0300

    01GettingStarted - Course Conclusion

--- 01GettingStarted/MyFirstProgram/Program.cs
+++ 01GettingStarted/MyFirstProgram/Program.cs
@@ -1,29 +1,43 @@
 ﻿using System;
+//importando uma classe específica
+//using WeatherUtilities = MyUtilities.WeatherUtilities;
+
+//importando todas as classes
+using MyUtilities;
 
 namespace MyFirstProgram
 {
 
-  class Temperature
+  /*
+class Temperature
+{
+
+  static float FahrenheitToCelsius(float temperatureFahrenheit)
   {
-    static float FahrenheitToCelsius(float temperatureFahrenheit)
-    {
 
-      //Casting usando float na frente da expressão para usar como float
-      //float temperatureCelsius = (float)((temperatureFahrenheit - 32) / 1.8);
+    //Casting usando float na frente da expressão para usar como float
+    //float temperatureCelsius = (float)((temperatureFahrenheit - 32) / 1.8);
 
-      //Podemos usar a inferência de tipo utilizando o var invés do tipo já que os outros parâmetros já tem tipo
-      var temperatureCelsius = (float)((temperatureFahrenheit - 32) / 1.8f);
+    //Podemos usar a inferência de tipo utilizando o var invés do tipo já que os outros parâmetros já tem tipo
+    var temperatureCelsius = (float)((temperatureFahrenheit - 32) / 1.8f);
 
-      return temperatureCelsius;
-    }
+    return temperatureCelsius;
   }
+}
+  */
 
-  class Program
+  class CheckComfort
   {
     static void Main(string[] args)
     {
       Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine("Hello World!");
+
+      Console.WriteLine("Where should we go in May?");
+      WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelsius(65), 73);
+      WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
+      WeatherUtilities.Report("São Paulo", 23, 65);
+
     }
   }
 }
--- 01GettingStarted/MyFirstProgram/WeatherUtilities.cs
+++ 01GettingStarted/MyFirstProgram/WeatherUtilities.cs
@@ -4,12 +4,12 @@ namespace MyUtilities
 {
   class WeatherUtilities
   {
-    static float FahrenheitToCelsius(float temperatureFahrenheit)
+    public static float FahrenheitToCelsius(float temperatureFahrenheit)
     {
-      return temperatureFahrenheit - 32 / 1.8f;
+      return (temperatureFahrenheit - 32) / 1.8f;
     }
 
-    static float CelsiusToFahrenheit(float temperatureCelsius)
+    public static float CelsiusToFahrenheit(float temperatureCelsius)
     {
       return temperatureCelsius * 1.8f + 32;
     }
@@ -17,13 +17,13 @@ namespace MyUtilities
     /*
      * Tho higher the index, the lower the confort
      */
-    static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+    private static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
     {
       //Probably not a very reliable formula:
       return (temperatureFahrenheit + humidityPercent) / 4;
     }
 
-    static void Report(string location, float temperatureCelsius, float humidity)
+    public static void Report(string location, float temperatureCelsius, float humidity)
     {
       var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
       Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
