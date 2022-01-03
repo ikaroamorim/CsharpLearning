1e261d313a63daf1d09a59e571d8b402b825770a
ikaroamorim <ikaro.amorim@gmail.com>
2022-01-03 00:36:51 -0300

    Criando um projeto com o básico dos conceitos.

---
+++ IkaroSchool/IkaroSchool/Classes/Aluno.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IkaroSchool.Classes
+{
+  public class Aluno : IComparable
+  {
+    protected static int currentId { get; set; }
+
+    protected int Id { get; set; }
+    protected string Nome { get; set; }
+    protected string Cpf { get; set; }
+    protected string Telefone { get; set; }
+    protected Status StatusAluno { get; }
+    protected DateTime DataMatricula { get; set; }
+
+    public Aluno(string nome, string cpf, string telefone, Status statusAluno)
+    {
+      currentId++;
+      Id = currentId;
+      Nome = nome;
+      Cpf = cpf;
+      Telefone = telefone;
+      StatusAluno = statusAluno;
+      DataMatricula = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+      
+      /* Esse foreach não funciona, nenhuma das props é pública
+      foreach (PropertyInfo prop in typeof(Aluno).GetProperties())
+      {
+        Console.WriteLine($"{prop.Name} = {prop.GetValue(this)}\n");
+      }
+      */
+      
+      return $"Id = {Id} \nNome = {Nome} \nCpf = {Cpf} \nTelefone = {Telefone} \nStatus = {StatusAluno} \nData de Matrícula = {DataMatricula.ToShortDateString()}";
+    }
+
+    public int CompareTo(object obj)
+    {
+      var alunoObj = (Aluno)obj;
+      if(Id > alunoObj.Id)
+      {
+        return 1;
+      }else if(Id< alunoObj.Id)
+      {
+        return -1;
+      }
+      else
+      {
+        return 0;
+      }
+    }
+
+    public enum Status
+    {
+      Analise,
+      Matriculado,
+      MensalidadeAtraso,
+      Encerrado,
+      Cancelado
+    }
+  }
+}
---
+++ IkaroSchool/IkaroSchool/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using IkaroSchool.Classes;
+
+namespace IkaroSchool
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+      Console.WriteLine("Bem vindo, Ikaro School");
+      Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+      string option = "0";
+
+      List<Aluno> listaAlunos = new List<Aluno>();
+
+
+
+
+      do
+      {
+        Console.WriteLine("\nSelecione uma opção");
+        Console.WriteLine("1 - Adicionar Aluno");
+        Console.WriteLine("2 - Exibe Aluno");
+        Console.WriteLine("s - Para encerrar o programa!");
+        option = Console.ReadLine();
+
+        switch (option)
+        {
+          case "1":
+            AddNewAluno(listaAlunos);
+            break;
+          case "2":
+            ListAllAlunos(listaAlunos);
+            break;
+          default:
+            break;
+        }
+
+      } while (option.ToLower() != "s");
+    }
+
+    private static void ListAllAlunos(List<Aluno> lista)
+    {
+      foreach (Aluno item in lista)
+      {
+        Console.WriteLine(item.ToString());
+      }
+    }
+
+    private static void AddNewAluno(List<Aluno> lista)
+    {
+      string nome, cpf, telefone;
+
+      Console.WriteLine("Digite o nome do aluno:");
+      nome = Console.ReadLine();
+      Console.WriteLine("Digite o cpf do aluno:");
+      cpf = Console.ReadLine();
+      Console.WriteLine("Digite o telefone do aluno:");
+      telefone = Console.ReadLine();
+
+      Aluno aluno = new Aluno(nome, cpf, telefone, Aluno.Status.Analise);
+
+      lista.Add(aluno);
+
+      Console.WriteLine($"A lista de aluno tem {lista.Count}");
+    }
+  }
+}
